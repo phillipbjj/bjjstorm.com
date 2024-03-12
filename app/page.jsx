353ff3dd686ad22d0e40a63fdc1abc3cc523b04c
phillipbjj <pjdempsey3@gmail.com>
2024-03-12 11:18:42 -0500

    switched from typescript to javascript

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// app/page.jsx
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>BJJ Storm</h1>
+    </div>
+  );
+}
