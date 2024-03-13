523955c45879b046e81bf24d315703900329e62c
= <=>
2024-03-12 22:54:00 -0500

    final attempt

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,19 @@
 // app/page.jsx
-import styles from './globals.css';
+import styles from './globals.css'
 import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.contentWrapper}>
-      <h1 className={styles.title}>BJJ Storm</h1>
-      <Image 
-        src="/monsterSleeping.jpg"
-        alt="Monster sleeping"
-        width={500}
-        height={300}
-        className={styles.image}
-      />
-      </div>
+        <h1 className={styles.title}>BJJ Storm</h1>
+        <Image 
+          src="/monsterSleeping.jpg"
+          alt="Monster sleeping"
+          width={500}
+          height={300}
+          className={styles.image}
+        />
     </div>
   );
 }
