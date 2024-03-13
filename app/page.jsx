d12b8d3359dc3c58b584db13816be3ce717b9b90
phillipbjj <pjdempsey3@gmail.com>
2024-03-13 08:51:25 -0500

    centered cat&h1, removed "styles." import

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,17 @@
-// app/page.jsx
-import styles from './globals.css'
+/*import styles from './globals.css'*/
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-        <h1 className={styles.title}>BJJ Storm</h1>
+    <div className="container">
+        <h1>BJJ Storm</h1>
         <Image 
+          className="image"
           src="/monsterSleeping.jpg"
           alt="Monster sleeping"
           width={500}
           height={300}
-          className={styles.image}
         />
     </div>
   );
