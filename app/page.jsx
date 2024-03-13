0edf410ce1ae6184b381f655d74ff73a09c07769
= <=>
2024-03-12 22:11:58 -0500

    created images folder, imported monsterSleeping to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>BJJ Storm</h1>
+      <Image 
+        src="/images/monsterSleeping.jpg"
+        alt="Monster sleeping "
+        width={500}
+        height={300}
+      />
     </div>
   );
 }
