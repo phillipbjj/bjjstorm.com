30a467754e55600e5c8e067f1d49b7b0928dc0ad
phillipbjj <pjdempsey3@gmail.com>
2024-03-26 17:42:38 -0500

    started page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 /*import styles from './globals.css'*/
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 export default function Home() {
   return (
+  <body>
     <div className="container">
         <h1>BJJ Storm</h1>
         <Image 
@@ -14,5 +16,15 @@ export default function Home() {
           height={300}
         />
     </div>
+    <div className="topRightLinks">
+      <h2>
+      <Link href="/nearbygyms"
+      <a>GYMS NEAR YOU!</a>
+        /*<a href="/map">SHOP</a>
+        <a href="/map">BJJ AI MODEL</a>*/
+      </Link>
+      </h2>
+    </div>
+  </body>
   );
 }
